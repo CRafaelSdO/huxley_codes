9724426c4b5cb8d532c4a154fb4e0f935389df4a
crafaelsdo <crafaelsdo@gmail.com>
2018-08-16 12:14:59 -0300

    Teste

--- src/2 - 3 Numeros em Ordem Crescente.c
+++ src/2 - 3 Numeros em Ordem Crescente.c
@@ -1,28 +1,25 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	int n[3];
 
 	scanf("%d%d%d", &n[0], &n[1], &n[2]);
-
-	if (n[0] >= n[1] && n[0] >= n[2]) {
+	
+	if(n[0] >= n[1] && n[0] >= n[2]) {
 		int aux = n[0];
 		n[0] = n[2];
 		n[2] = aux;
-	} else if (n[1] >= n[0] && n[1] >= n[2]) {
+	} else if(n[1] >= n[0] && n[1] >= n[2]) {
 		int aux = n[1];
 		n[1] = n[2];
 		n[2] = aux;
 	}
-
-	if (n[0] > n[1]) {
+	if(n[0] > n[1]) {
 		int aux = n[0];
 		n[0] = n[1];
 		n[1] = aux;
 	}
 
 	printf("%d\n%d\n%d\n", n[0], n[1], n[2]);
-	
 	return 0;
 }
